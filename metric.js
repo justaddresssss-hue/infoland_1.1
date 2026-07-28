ea1b8826ed85955a58348001cc63b572d0b5e729
justaddresssss-hue <just.addresssss@gmail.com>
2026-07-28 15:49:09 +0300

    Replace Yandex.Metrika counter implementation

--- metric.js
+++ metric.js
@@ -1,28 +1,13 @@
-const METRICA_COUNTER_ID = XXXXXX; 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=111096166', 'ym');
 
-(function(m,e,t,r,i,k,a){
-    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {
-        if (document.scripts[j].src === r) { return; }
-    }
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-ym(METRICA_COUNTER_ID, "init", {
-    clickmap: true,
-    trackLinks: true,
-    accurateTrackBounce: true,
-    webvisor: true,
-    ecommerce: "dataLayer" // Раскомментируйте, если используете e-commerce
-});
-
-// [ШАБЛОН] Опционально: отслеживание кликов по конкретным элементам (например, "Перейти в каталог")
-document.addEventListener('DOMContentLoaded', () => {
-    const ctaButton = document.querySelector('.site-link');
-    if (ctaButton) {
-        ctaButton.addEventListener('click', () => {
-            ym(METRICA_COUNTER_ID, 'reachGoal', 'click_to_catalog');
-        });
-    }
-});
+    ym(111096166, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/111096166" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
